e8af6d4be2797d415ae75ecf9531f24cbd7bdf32
Grahame Bowland <grahame@oreamnos.com.au>
2022-04-05 19:13:27 +0800

    drop yarn for npm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './App.css';
-import { Table, Form, Row, Col, Input, Badge } from 'reactstrap';
-import Books from './books.json';
+import React, { useState, useRef, useEffect } from "react";
+import "./App.css";
+import { Table, Form, Row, Col, Input, Badge } from "reactstrap";
+import Books from "./books.json";
 
 function useInput() {
     const [value, setValue] = useState<string>("");
@@ -11,17 +11,25 @@ function useInput() {
         inputReference.current?.focus();
     }, []);
 
-    const input = <Input innerRef={inputReference} className="bg-dark text-light" value={value} onChange={e => setValue(e.target.value)} type='text' />;
+    const input = (
+        <Input
+            innerRef={inputReference}
+            className="bg-dark text-light"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+        />
+    );
     return [value, input];
 }
 
 interface BookProp {
-    books: typeof Books.books
-};
+    books: typeof Books.books;
+}
 
 function filterBook(book: typeof Books.books[0], re: RegExp) {
     return re.test(book.author) || re.test(book.title) || re.test(book.loc);
-};
+}
 
 function BookTable(props: React.PropsWithChildren<BookProp>) {
     const tableRows = () => {
@@ -36,21 +44,21 @@ function BookTable(props: React.PropsWithChildren<BookProp>) {
             </tr>
         ));
     };
-    return <Table dark striped>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>DDC</th>
-                <th>Author</th>
-                <th>Title</th>
-                <th>ISBN</th>
-                <th>Date</th>
-            </tr>
-        </thead>
-        <tbody>
-            {tableRows()}
-        </tbody>
-    </Table>;
+    return (
+        <Table dark striped>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>DDC</th>
+                    <th>Author</th>
+                    <th>Title</th>
+                    <th>ISBN</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>{tableRows()}</tbody>
+        </Table>
+    );
 }
 
 function App() {
@@ -58,27 +66,27 @@ function App() {
     var error = "";
     var re: RegExp;
     try {
-        re = new RegExp(searchString.toString(), 'i');
-    } catch (e) {
+        re = new RegExp(searchString.toString(), "i");
+    } catch (e: any) {
         error = e.toString();
-        re = new RegExp('^.*$', 'i');
+        re = new RegExp("^.*$", "i");
     }
-    const book_rows = Books.books.filter(book => filterBook(book, re));
+    const book_rows = Books.books.filter((book) => filterBook(book, re));
 
-    return <div>
-        <Form>
-            <Row className="bg-dark">
-                <Col xs={{ size: 9, offset: 1 }}>
-                    {searchStringInput}
-                </Col>
-                <Col xs={{ size: 1 }}>
-                    <Badge pill>{book_rows.length}</Badge>
-                </Col>
-            </Row>
-            {error}
-        </Form>
-        <BookTable books={book_rows} />
-    </div>;
+    return (
+        <div>
+            <Form>
+                <Row className="bg-dark">
+                    <Col xs={{ size: 9, offset: 1 }}>{searchStringInput}</Col>
+                    <Col xs={{ size: 1 }}>
+                        <Badge pill>{book_rows.length}</Badge>
+                    </Col>
+                </Row>
+                {error}
+            </Form>
+            <BookTable books={book_rows} />
+        </div>
+    );
 }
 
 export default App;
