5a99dff7bd6d5a30461e19e608b647a0087abe4e
Grahame Bowland <grahame@bowland.au>
2025-07-08 01:29:43 +0800

    spec fic complete

---
+++ data/fixup.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+for i in cookbooks.txt crime.txt liturgy.txt poetry-and-epic-myth.txt romance.txt speculative-fiction.txt;
+        echo $i
+        cat "$i" | sort | uniq > "$i".n && mv "$i".n "$i"
+end
