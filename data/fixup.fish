d7ad37840b44276014a09398fab6f2fa078a1d38
Grahame Bowland <grahame@bowland.au>
2025-07-08 17:16:55 +0800

    wip

--- data/fixup.fish
+++ data/fixup.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-for i in cookbooks.txt crime.txt first-nations.txtliturgy.txt poetry-and-epic-myth.txt romance.txt speculative-fiction.txt;
+for i in cookbooks.txt crime.txt first-nations.txt liturgy.txt poetry-and-epic-myth.txt romance.txt speculative-fiction.txt;
         echo $i
         cat "$i" | sort | uniq > "$i".n && mv "$i".n "$i"
 end
